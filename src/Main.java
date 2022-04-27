c11edd6587665cd688e81e1546e92e6f1cc0ecd7
Ferniss <mathiasf2000@hotmail.com>
2022-04-27 08:43:54 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/GUI/View/FrontPage.fxml"));
-        primaryStage.setTitle("Event Management");
+        primaryStage.setTitle("SOSU System");
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
         primaryStage.show();
