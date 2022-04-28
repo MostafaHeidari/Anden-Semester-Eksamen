cc061791c5a1594d7d44b715186c46cc4d9b7516
Ferniss <mathiasf2000@hotmail.com>
2022-04-28 09:59:21 +0200

    controller is up and Login btn goes to patient

--- src/GUI/Controller/FrontPageController.java
+++ src/GUI/Controller/FrontPageController.java
@@ -1,8 +1,23 @@
 package GUI.Controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class FrontPageController {
-    public void Login(ActionEvent actionEvent) {
+    @FXML
+    private Button btnLogin;
+    public void Login() throws IOException {
+        Stage switcher = (Stage) btnLogin.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/View/Patient.fxml"));
+        switcher.setTitle("AdminManagement");
+        Scene scene = new Scene(root);
+        switcher.setScene(scene);
     }
 }
