5858ddf68f179041b635942c92374a0dd6dda493
FrederikReigstad <Frederik9876@gmail.com>
2022-04-28 15:25:36 +0200

    sørget for at enter også logering med knappen

--- src/GUI/Controller/LoginController.java
+++ src/GUI/Controller/LoginController.java
@@ -11,6 +11,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -41,6 +43,11 @@ public class LoginController {
             switcher.setTitle("Patient");
             Scene scene = new Scene(root);
             switcher.setScene(scene);
+        }
+    }
+    public void handleOnKeyPressed(KeyEvent keyEvent) throws IOException, SQLServerException {
+        if (keyEvent.getCode().equals(KeyCode.ENTER)) {
+            Login();
         }
     }
 }
