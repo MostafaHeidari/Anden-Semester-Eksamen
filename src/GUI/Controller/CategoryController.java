f5495456dcf18f3533afe09c01487b7bd1533df6
Ferniss <mathiasf2000@hotmail.com>
2022-05-02 16:23:31 +0200

    window get to open

--- src/GUI/Controller/CategoryController.java
+++ src/GUI/Controller/CategoryController.java
@@ -1,9 +1,23 @@
 package GUI.Controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CategoryController {
-    public void Test(MouseEvent mouseEvent) {
-
+    @FXML
+    private javafx.scene.layout.HBox HBox;
+    public void Test(MouseEvent mouseEvent) throws IOException {
+        Stage switcher = (Stage) HBox.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/View/Patient.fxml"));
+        Scene scene = new Scene(root);
+        switcher.setTitle("SOSU System");
+        switcher.setScene(scene);
     }
 }
