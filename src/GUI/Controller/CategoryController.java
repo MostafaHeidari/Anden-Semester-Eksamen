9608a54a3710ecf83679a4647fe1a25dabf74dd3
Ferniss <mathiasf2000@hotmail.com>
2022-05-02 17:02:37 +0200

    Has 2 test methods for windows

--- src/GUI/Controller/CategoryController.java
+++ src/GUI/Controller/CategoryController.java
@@ -13,9 +13,21 @@ import java.io.IOException;
 public class CategoryController {
     @FXML
     private javafx.scene.layout.HBox HBox;
+
+    @FXML
+    private javafx.scene.layout.HBox HBox1;
+
     public void Test(MouseEvent mouseEvent) throws IOException {
         Stage switcher = (Stage) HBox.getScene().getWindow();
         Parent root = FXMLLoader.load(getClass().getResource("/GUI/View/Patient.fxml"));
+        Scene scene = new Scene(root);
+        switcher.setTitle("SOSU System");
+        switcher.setScene(scene);
+    }
+
+    public void Test1(MouseEvent mouseEvent) throws IOException {
+        Stage switcher = (Stage) HBox1.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/View/OpretBorger.fxml"));
         Scene scene = new Scene(root);
         switcher.setTitle("SOSU System");
         switcher.setScene(scene);
