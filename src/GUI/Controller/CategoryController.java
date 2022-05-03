af3a5903cd95876f10c84685cf3c0de2a9f40c85
nicklas0110 <91014438+nicklas0110@users.noreply.github.com>
2022-05-03 14:30:47 +0200

    Category stuff

--- src/GUI/Controller/CategoryController.java
+++ src/GUI/Controller/CategoryController.java
@@ -35,4 +35,139 @@ public class CategoryController {
         switcher.setTitle("SOSU System");
         switcher.setScene(scene);
     }
+
+    public void funktionsniveau_1(MouseEvent mouseEvent) {
+    }
+
+    public void funktionsniveau_2(MouseEvent mouseEvent) {
+    }
+
+    public void bevægeapparat_1(MouseEvent mouseEvent) {
+    }
+
+    public void seksualitet_1(MouseEvent mouseEvent) {
+    }
+
+    public void hudogslimhinder_1(MouseEvent mouseEvent) {
+    }
+
+    public void hudogslimhinder_2(MouseEvent mouseEvent) {
+    }
+
+    public void hudogslimhinder_3(MouseEvent mouseEvent) {
+    }
+
+    public void hudogslimhinder_4(MouseEvent mouseEvent) {
+    }
+
+    public void hudogslimhinder_5(MouseEvent mouseEvent) {
+    }
+
+    public void hudogslimhinder_6(MouseEvent mouseEvent) {
+    }
+
+    public void hudogslimhinder_7(MouseEvent mouseEvent) {
+    }
+
+    public void hudogslimhinder_8(MouseEvent mouseEvent) {
+    }
+
+    public void hudogslimhinder_9(MouseEvent mouseEvent) {
+    }
+
+    public void respirationogcirkulation_1(MouseEvent mouseEvent) {
+    }
+
+    public void respirationogcirkulation_2(MouseEvent mouseEvent) {
+    }
+
+    public void ernæring_1(MouseEvent mouseEvent) {
+    }
+
+    public void ernæring_2(MouseEvent mouseEvent) {
+    }
+
+    public void ernæring_3(MouseEvent mouseEvent) {
+    }
+
+    public void ernæring_4(MouseEvent mouseEvent) {
+    }
+
+    public void ernæring_5(MouseEvent mouseEvent) {
+    }
+
+    public void smerterogsanseindtryk_1(MouseEvent mouseEvent) {
+    }
+
+    public void smerterogsanseindtryk_2(MouseEvent mouseEvent) {
+    }
+
+    public void smerterogsanseindtryk_3(MouseEvent mouseEvent) {
+    }
+
+    public void smerterogsanseindtryk_4(MouseEvent mouseEvent) {
+    }
+
+    public void smerterogsanseindtryk_5(MouseEvent mouseEvent) {
+    }
+
+    public void smerterogsanseindtryk_6(MouseEvent mouseEvent) {
+    }
+
+    public void smerterogsanseindtryk_7(MouseEvent mouseEvent) {
+    }
+
+    public void smerterogsanseindtryk_8(MouseEvent mouseEvent) {
+    }
+
+    public void søvnoghvile_1(MouseEvent mouseEvent) {
+    }
+
+    public void søvnoghvile_2(MouseEvent mouseEvent) {
+    }
+
+    public void videnogudvikling_1(MouseEvent mouseEvent) {
+    }
+
+    public void videnogudvikling_2(MouseEvent mouseEvent) {
+    }
+
+    public void videnogudvikling_3(MouseEvent mouseEvent) {
+    }
+
+    public void videnogudvikling_4(MouseEvent mouseEvent) {
+    }
+
+    public void videnogudvikling_5(MouseEvent mouseEvent) {
+    }
+
+    public void kommunikation_1(MouseEvent mouseEvent) {
+    }
+
+    public void psykosocialeforhold_1(MouseEvent mouseEvent) {
+    }
+
+    public void psykosocialeforhold_2(MouseEvent mouseEvent) {
+    }
+
+    public void psykosocialeforhold_3(MouseEvent mouseEvent) {
+    }
+
+    public void psykosocialeforhold_4(MouseEvent mouseEvent) {
+    }
+
+    public void udskillelseafaffaldsstoffer_1(MouseEvent mouseEvent) {
+    }
+
+    public void udskillelseafaffaldsstoffer_2(MouseEvent mouseEvent) {
+    }
+
+    public void udskillelseafaffaldsstoffer_3(MouseEvent mouseEvent) {
+    }
+
+    public void udskillelseafaffaldsstoffer_4(MouseEvent mouseEvent) {
+    }
+
+    public void udskillelseafaffaldsstoffer_5(MouseEvent mouseEvent) {
+    }
 }
