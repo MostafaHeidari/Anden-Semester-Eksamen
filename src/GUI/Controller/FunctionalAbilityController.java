a1f36d085a4e68d3eee97150e332361b2a4659c5
nicklas0110 <91014438+nicklas0110@users.noreply.github.com>
2022-05-05 10:45:28 +0200

    functional ability checkmarks

--- src/GUI/Controller/FunctionalAbilityController.java
+++ src/GUI/Controller/FunctionalAbilityController.java
@@ -7,5 +7,40 @@ import javafx.scene.control.Tooltip;
 
 public class FunctionalAbilityController {
 
-    
+
+    public void Checked1(ActionEvent actionEvent) {
+    }
+
+    public void Checked2(ActionEvent actionEvent) {
+    }
+
+    public void Checked3(ActionEvent actionEvent) {
+    }
+
+    public void Checked4(ActionEvent actionEvent) {
+    }
+
+    public void Checked5(ActionEvent actionEvent) {
+    }
+
+    public void Checked6(ActionEvent actionEvent) {
+    }
+
+    public void checked21(ActionEvent actionEvent) {
+    }
+
+    public void checked22(ActionEvent actionEvent) {
+    }
+
+    public void checked23(ActionEvent actionEvent) {
+    }
+
+    public void checked24(ActionEvent actionEvent) {
+    }
+
+    public void checked25(ActionEvent actionEvent) {
+    }
+
+    public void checked26(ActionEvent actionEvent) {
+    }
 }
