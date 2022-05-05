bd04d3fb80f6f75727b3aeb721232b715bc45c49
Ferniss <mathiasf2000@hotmail.com>
2022-05-05 09:37:24 +0200

    CategoryModel is done

--- src/GUI/Model/CategoryModel.java
+++ src/GUI/Model/CategoryModel.java
@@ -5,6 +5,7 @@ import BLL.LoginManager;
 import DAL.CategoryDAO;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class CategoryModel {
     private CategoryManager categoryManager;
@@ -15,5 +16,17 @@ public class CategoryModel {
      */
     public CategoryModel() throws IOException {
         categoryManager = new CategoryManager();
+    }
+
+    public void createCategory(int caseID, String ProblemNavn, String ProblemBeskrivelse) throws SQLException{
+        categoryManager.createCategory(caseID, ProblemNavn, ProblemBeskrivelse);
+    }
+
+    public String readCategory(int caseID, String ProblemNavn) throws SQLException{
+        return categoryManager.readCategory(caseID, ProblemNavn);
+    }
+
+    public void updateCategory(int caseID, String ProblemNavn, String ProblemBeskrivelse) throws SQLException{
+        categoryManager.updateCategory(caseID, ProblemNavn, ProblemBeskrivelse);
     }
 }
