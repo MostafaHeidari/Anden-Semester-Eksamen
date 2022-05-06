b29a626a5679373111dfb6cbe441589e429bec7d
Ferniss <mathiasf2000@hotmail.com>
2022-05-06 13:23:19 +0200

    New file in model has notes

--- src/GUI/Model/CitizenInfoModel.java
+++ src/GUI/Model/CitizenInfoModel.java
@@ -12,10 +12,18 @@ public class CitizenInfoModel {
     private CitizenInfoManger citizenInfoManger;
     private ObservableList<Citizen> citizensList = FXCollections.observableArrayList();
 
+    /**
+     * Constructor
+     * @throws IOException
+     */
     public CitizenInfoModel() throws IOException {
         citizenInfoManger = new CitizenInfoManger();
     }
 
+    /**
+     * Gets the list of getAllCitizens using the getAllCitizens method in citizenInfoManger.
+     * @return a list of getAllCitizens
+     */
     public ObservableList getAllCitizens() {
         citizensList = citizenInfoManger.getAllCitizens();
         return citizensList;
