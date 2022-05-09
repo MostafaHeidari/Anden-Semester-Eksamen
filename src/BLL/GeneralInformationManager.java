6396c59c8d72140f7e0f4937bdc0a9ee02a39ef9
MostafaHeidari <70130828+MostafaHeidari@users.noreply.github.com>
2022-05-09 15:05:37 +0200

    bug fixed

--- src/BLL/GeneralInformationManager.java
+++ src/BLL/GeneralInformationManager.java
@@ -1,5 +1,13 @@
 package BLL;
 
+ import DAL.GeneralInformationDAO;
+
 public class GeneralInformationManager {
+
+    private GeneralInformationDAO generalInformationDAO;
+
+    public GeneralInformationManager(GeneralInformationDAO generalInformationDAO) {
+        this.generalInformationDAO = generalInformationDAO;
+    }
 
 }
