f03869a13182dea6a2c1bb4a1f9da7f83ea0ef8b
MostafaHeidari <70130828+MostafaHeidari@users.noreply.github.com>
2022-05-12 16:41:56 +0200

    created method in createCitizen class

--- src/DAL/CitizenInfoDAO.java
+++ src/DAL/CitizenInfoDAO.java
@@ -2,12 +2,10 @@ package DAL;
 
 import BE.CitizenInfo;
 import DAL.db.DatabaseConnector;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,7 +35,7 @@ public class CitizenInfoDAO {
         String sql = "SELECT * FROM Patients";
         Statement statement = con.createStatement();
         ResultSet rs = statement.executeQuery(sql);
-        while (rs.next()) { // Creates and adds song objects into an array list
+        while (rs.next()) {
             CitizenInfo citizenInfoCord = new CitizenInfo(
                     rs.getInt("PatientID"),
                     rs.getString("PatientName"),
@@ -50,6 +48,31 @@ public class CitizenInfoDAO {
             allCitizenInfos.add(citizenInfoCord);
         }
         return allCitizenInfos;
+    }
 
+    public CitizenInfo createCitizen(String citizenName, String citizenLastName, String citizenAge, int phoneNumber, String address, String sex, String information) throws SQLException {
+        try (Connection connection = DC.getConnection()) {
+            String sql = "INSERT INTO Citizen (firstName, lastName , SSN, address, sex, info) VALUES (?,?,?,?,?,?);";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+                preparedStatement.setString(1, citizenName);
+                preparedStatement.setString(2, citizenLastName);
+                preparedStatement.setString(3, citizenAge);
+                preparedStatement.setInt(4, phoneNumber);
+                preparedStatement.setString(5, address);
+                preparedStatement.setBoolean(6, Boolean.parseBoolean(sex));
+                preparedStatement.setString(7, information);
+                preparedStatement.execute();
+                ResultSet resultSet = preparedStatement.getGeneratedKeys();
+                int id = 0;
+                if (resultSet.next()) {
+                    id = resultSet.getInt(1);
+                }
+
+                CitizenInfo citizen = new CitizenInfo(id, citizenName, citizenLastName, citizenAge, phoneNumber,address, sex, information);
+                return citizen;
+            }
+        } catch (SQLServerException throwables) {
+            throw new SQLException();
+        }
     }
 }
