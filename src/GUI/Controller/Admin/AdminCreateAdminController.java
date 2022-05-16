dbc3ad91a8d6e6cbaa15d4077a889c35d51c2a88
FrederikReigstad <Frederik9876@gmail.com>
2022-05-16 10:54:26 +0200

    Admin abilty to create a admin

--- src/GUI/Controller/Admin/AdminCreateAdminController.java
+++ src/GUI/Controller/Admin/AdminCreateAdminController.java
@@ -1,4 +1,26 @@
 package GUI.Controller.Admin;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class AdminCreateAdminController {
+    public JFXButton BtnBack;
+    public TextField adminNameTxt;
+    public TextField adminUserNameTxt;
+    public PasswordField adminPasswordTxt;
+    public Button createAdmin;
+    public TableView tvAdmin;
+    public TableColumn tcAdminName;
+    public TableColumn tcAdminUserName;
+    public JFXButton deleteAdmin;
+
+    public void BtnBack(ActionEvent actionEvent) {
+    }
+
+    public void createAdminBtn(ActionEvent actionEvent) {
+    }
+
+    public void deleteAdminBtn(ActionEvent actionEvent) {
+    }
 }
