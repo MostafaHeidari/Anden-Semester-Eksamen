e9ea3d829fe0245db95d17317b8fcd0fd2e5338e
Ferniss <mathiasf2000@hotmail.com>
2022-05-19 08:13:54 +0200

    Working on test

--- test/GUI/Controller/Universal/LoginControllerTest.java
+++ test/GUI/Controller/Universal/LoginControllerTest.java
@@ -1,11 +1,26 @@
 package GUI.Controller.Universal;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 class LoginControllerTest {
 
     @Test
-    void login() {
+    void login() throws IOException {
+        //Triple A pattern
+
+        //Arrange - setup our test objects etc.
+        LoginController Login = new LoginController();
+
+        // Act - do the actual calc or method run
+        //Student
+        boolean actualValue = Login.equals("Student");
+
     }
 
     @Test
