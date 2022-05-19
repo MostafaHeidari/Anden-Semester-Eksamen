218587d6cb4716ebb2c738faf1a12cbdfad6c176
Ferniss <mathiasf2000@hotmail.com>
2022-05-19 07:58:56 +0200

    Junit test

---
+++ test/GUI/Controller/Universal/LoginControllerTest.java
@@ -0,0 +1,14 @@
+package GUI.Controller.Universal;
+
+import org.junit.jupiter.api.Test;
+
+class LoginControllerTest {
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void handleOnKeyPressed() {
+    }
+}
