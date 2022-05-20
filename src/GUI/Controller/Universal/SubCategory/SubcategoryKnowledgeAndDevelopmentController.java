abfb2b385de7a9f5fccb6f61d82ba8029bb87a5e
Ferniss <mathiasf2000@hotmail.com>
2022-05-20 08:26:41 +0200

    SubcategoryKnowledgeAndDevelopment gui and controller

---
+++ src/GUI/Controller/Universal/SubCategory/SubcategoryKnowledgeAndDevelopmentController.java
@@ -0,0 +1,108 @@
+package GUI.Controller.Universal.SubCategory;
+
+import BE.CitizenInfo;
+import GUI.Controller.Universal.CategoryController;
+import GUI.Model.CategoryModel;
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class SubcategoryKnowledgeAndDevelopmentController {
+    private CitizenInfo selectedCitizenInfo;
+
+
+    @FXML
+    private JFXButton btnBack;
+    @FXML
+    private JFXButton btnLogOut;
+
+    public Text subCatText;
+    public TextArea txtDayProblems;
+    public TextArea txtSleepProblems;
+
+    // this is instance variable is not used to now//
+    private int caseID = -1;
+
+    CategoryModel categoryModel = new CategoryModel();
+
+    /**
+     * Constructor
+     * @throws IOException
+     */
+    public SubcategoryKnowledgeAndDevelopmentController() throws IOException, SQLException {
+
+    }
+
+    /**
+     * Goes to Category view
+     */
+
+    public void subCategoryBack(ActionEvent actionEvent) throws IOException {
+        Stage switcher = (Stage) btnBack.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/View/Universal/Category.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+
+        CategoryController controller = loader.getController();
+        controller.setCaseID(caseID);
+        controller.setSelectedCitizen(selectedCitizenInfo);
+
+        switcher.setTitle("SOSU System");
+        switcher.setScene(scene);
+    }
+
+    /**
+     * Goes to Login view
+     */
+    public void subCategoryLogOut(ActionEvent actionEvent) throws IOException {
+        Stage switcher = (Stage) btnLogOut.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/View/Universal/Login.fxml"));
+        Scene scene = new Scene(root);
+        switcher.setTitle("SOSU System");
+        switcher.setScene(scene);
+    }
+
+    /**
+     * this button is used to save the information to databassen. if the databassen is empty
+     * @param actionEvent
+     * @throws SQLException
+     */
+    public void subCategorySave(ActionEvent actionEvent) throws SQLException {
+        if (categoryModel.readCategory(caseID,"Day problems") == null){
+
+            categoryModel.createCategory(caseID,"Day problems",txtDayProblems.getText());
+        }
+        categoryModel.updateCategory(caseID,"Day problems",txtDayProblems.getText());
+
+        if (categoryModel.readCategory(caseID,"Sleep problems") == null){
+
+            categoryModel.createCategory(caseID,"Sleep problems",txtSleepProblems.getText());
+        }
+        categoryModel.updateCategory(caseID,"Sleep problems",txtSleepProblems.getText());
+    }
+
+    /**
+     * this method is used to sende information from en controller to other controller
+     * @throws SQLException
+     */
+
+    public void setId(int caseID) throws SQLException {
+        this.caseID = caseID;
+        txtDayProblems.setText(categoryModel.readCategory(caseID,"Day problems"));
+        txtSleepProblems.setText(categoryModel.readCategory(caseID,"Sleep problems"));
+    }
+
+    public void setSelectedCitizen(CitizenInfo selectedCitizen) {
+        selectedCitizenInfo = selectedCitizen;
+    }
+
+}
