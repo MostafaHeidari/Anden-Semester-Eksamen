0961b7e7162b58ac69dda661cff2e3538a74b7ad
MostafaHeidari <70130828+MostafaHeidari@users.noreply.github.com>
2022-05-27 10:51:07 +0200

    back button in GeneralinformationController make an action

--- src/GUI/Controller/Universal/GeneralinformationController.java
+++ src/GUI/Controller/Universal/GeneralinformationController.java
@@ -1,9 +1,16 @@
 package GUI.Controller.Universal;
 
 import BE.CitizenInfo;
+import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class GeneralinformationController {
     @FXML
@@ -28,11 +35,18 @@ public class GeneralinformationController {
     public Button BtnBolige;
     @FXML
     public Button BtnNetwork;
+    @FXML
+    public JFXButton BtnBack;
 
     private CitizenInfo selectedCitizenInfo;
 
 
-    public void BtnBack(ActionEvent actionEvent) {
+    public void BtnBack(ActionEvent actionEvent) throws IOException {
+        Stage switcher = (Stage) BtnBack.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/View/Universal/Case/Cases.fxml"));
+        Scene scene = new Scene(root);
+        switcher.setTitle("");
+        switcher.setScene(scene);
     }
 
     public void btnHandleSaveCitizen(ActionEvent actionEvent) {
