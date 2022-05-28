f391d221636da5dad0e18d36ac5f88e9d26d38b3
nicklas0110 <91014438+nicklas0110@users.noreply.github.com>
2022-05-28 15:04:57 +0200

    icon

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,5 +13,8 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root));
         //primaryStage.setResizable(false);
         primaryStage.show();
+
+        Image icon = new Image(getClass().getResourceAsStream("/GUI/View/img/old lady.png"));
+        primaryStage.getIcons().add(icon);
     }
 }
